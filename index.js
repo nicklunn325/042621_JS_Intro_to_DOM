725947e00358f1ec1b027750e93ce002e93a98a4
flatlandnick <nicholas.lunn@flatironschool.com>
2021-05-05 11:18:10 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let header = document.querySelector("h1") // no . or # will search by tag name
+
+let list = document.querySelector(".swbats") // . denotes search by class
+
+let image = document.querySelector("#dom") // # denotes search by id
+
+let listElements = document.querySelectorAll("li")
+
+// we cannot call all array methods on collection(nodeList) returned by querySelectorAll
